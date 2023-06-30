a4d2252c840992fe58775235d4a69faaa79c2afa
Abdul Rehman Khawar <designbysoil@gmail.com>
2023-06-30 12:17:59 -0700

    New version of the portfolio.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+	// Slick Slider
+	$('.slideshow').slick({
+		infinite: true,
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: false,
+		prevArrow: false,
+		nextArrow: false,
+		fade: true,
+  		cssEase: 'linear'
+	});
+
+});
